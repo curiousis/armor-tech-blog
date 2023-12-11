c0a37172d5dbfa3bbc7faa5a65a464fc5829161b
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-12-11 12:34:26 +0200

    Adding some styling to header and hero, and adding the register file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Link } from '@mui/material';
+import { orange } from '@mui/material/colors';
 
 export default function Header() {
   return (
-    <header className='flex w-full justify-between py-4 px-3'>
-        <div>Armor-Tech</div>
+    <AppBar position="static" sx={{ backgroundColor: '#2C3E50' }}>
+      <Toolbar className='flex justify-between py-4 px-3'>
+        <Typography variant="h6" style={{color:orange[500]}} component="div">
+          ArmorSphere
+        </Typography>
         <nav className='w-1/2'>
-            <ul className='flex justify-around'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+          <ul className='flex justify-around'>
+            <li><Link color="inherit" href="#" underline='none'>Home</Link></li>
+            <li><Link color="inherit" href="#" underline='none'>About</Link></li>
+            <li><Link color="inherit" href="#" underline='none'>Contact</Link></li>
+          </ul>
         </nav>
-    </header>
-  )
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Container,Typography,Button } from '@mui/material'
+import React from 'react';
+import { Container, Typography, Button } from '@mui/material';
+import { orange } from '@mui/material/colors';
 
 export default function Hero() {
   return (
-    <Container sx={{display:'flex',flexDirection:'column',width:'100%',backgroundColor:'#2C3E50',padding:'2rem',alignItems:'center',gap:'2rem'}}>
-        <Typography variant='h2' component='h1' fontWeight={'bold'} color='#F4F6F6'>Welcome to Armor-Tech</Typography>
-        <Typography component='p' color='#F4F6F6' fontSize='1.2rem'>
-            Unlocking Innovation, Ensuring Security
-            At Armor-Tech, we redefine the boundaries of technology to safeguard your world
-        </Typography>
-        <div className='flex justify-center items-center gap-4 w-1/2'>
-            <button type='button' className='text-slate-700 bg-amber-500 px-4 py-2 rounded-xl font-bold'>Login</button>
-            <button type='button' className='text-slate-700 bg-slate-50 px-4 py-2 rounded-xl font-bold'>Register</button>
-        </div>
-
+    <Container sx={{ display: 'flex', flexDirection: 'column', width: '100%', backgroundColor: '#2C3E50', padding: '2rem', alignItems: 'center', gap: '2rem' }}>
+      <Typography variant='h2' component='h1' fontWeight={'bold'} color='#F4F6F6'>Welcome to Armor-Tech</Typography>
+      <Typography component='p' color='#F4F6F6' fontSize='1.2rem'>
+        Unveiling Limitless Innovation, Safeguarding Your Digital Domain - Join us at Armor-Tech's micro-blog social hub, where we explore the cutting edge of technology. Your go-to platform for unlocking innovation and ensuring security in the dynamic world of digital possibilities.
+      </Typography>
+      <div className='flex justify-center items-center gap-4 w-1/2'>
+        <Button variant='contained' style={{ backgroundColor: orange[500], color: '#2C3E50' }}>Login</Button>
+        <Button variant='contained' color='warning' style={{backgroundColor:'#fff', color:'#2C3E50'}}>Register</Button>
+      </div>
     </Container>
-  )
+  );
 }
